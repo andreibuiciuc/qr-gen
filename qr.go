47e4fedf0b5a23ef9aaf4b37309f7fc4c6bb393f
Andrei Buiciuc <abuiciuc33@gmail.com>
2023-12-25 21:13:06 +0200

    implement mode indicator handling, refactor + tests

--- qr.go
+++ qr.go
@@ -5,31 +5,40 @@ import (
 	"regexp"
 )
 
-type QrCodeMode int
-type QrErrCorrectionLvl int
+type QrCodeMode string
+type QrErrCorrectionLvl string
 type QrVersion int
+type QrModeIndicator byte
+
+const INVALID_IDX = -1
+const EMPTY_STRING = ""
 
 const (
-	INVALID_MODE QrCodeMode = iota
-	NUMERIC
-	ALPHA_NUMERIC
-	BYTE
+	NUMERIC       QrCodeMode = "numeric"
+	ALPHA_NUMERIC QrCodeMode = "alpha"
+	BYTE          QrCodeMode = "byte"
 )
 
 const (
-	LOW QrErrCorrectionLvl = iota
-	MEDIUM
-	QUARTILE
-	HIGH
+	LOW      QrErrCorrectionLvl = "low"
+	MEDIUM   QrErrCorrectionLvl = "medium"
+	QUARTILE QrErrCorrectionLvl = "quartile"
+	HIGH     QrErrCorrectionLvl = "high"
 )
 
 const (
-	INVALID_VERSION QrVersion = iota
+	_ QrVersion = iota
 	VERSION_1
 	VERSION_2
 	VERSION_3
 	VERSION_4
 	VERSION_5
+)
+
+const (
+	NUMERIC_INDICATOR       QrModeIndicator = 0b0001
+	ALPHA_NUMERIC_INDICATOR QrModeIndicator = 0b0010
+	BYTE_INDICATOR          QrModeIndicator = 0b0100
 )
 
 var patterns = map[QrCodeMode]string{
@@ -71,6 +80,18 @@ var capacities = map[QrVersion]map[QrErrCorrectionLvl][]int{
 	},
 }
 
+var modeIndicators = map[QrCodeMode]QrModeIndicator{
+	NUMERIC:       NUMERIC_INDICATOR,
+	ALPHA_NUMERIC: ALPHA_NUMERIC_INDICATOR,
+	BYTE:          BYTE_INDICATOR,
+}
+
+var modeToIndex = map[QrCodeMode]int{
+	NUMERIC:       0,
+	ALPHA_NUMERIC: 1,
+	BYTE:          2,
+}
+
 func GetMode(s string) (QrCodeMode, error) {
 	if matched, _ := regexp.MatchString(patterns[NUMERIC], s); matched {
 		return NUMERIC, nil
@@ -84,25 +105,24 @@ func GetMode(s string) (QrCodeMode, error) {
 		return BYTE, nil
 	}
 
-	return QrCodeMode(0), fmt.Errorf("Invalid input pattern")
+	return QrCodeMode(EMPTY_STRING), fmt.Errorf("Invalid input pattern")
 }
 
-func GetSmallestVersion(s string, level QrErrCorrectionLvl) (QrVersion, error) {
+func GetSmallestVersion(s string, mode QrCodeMode, level QrErrCorrectionLvl) (QrVersion, error) {
 	version := 1
-	mode, err := GetMode(s)
-
-	if err != nil {
-		return 0, err
-	}
 
 	for version <= len(capacities) {
-		if len(s) <= capacities[QrVersion(version)][level][mode-1] {
+		if len(s) <= capacities[QrVersion(version)][level][modeToIndex[mode]] {
 			return QrVersion(version), nil
 		}
 		version += 1
 	}
 
-	return INVALID_VERSION, fmt.Errorf("Cannot compute QR version")
+	return QrVersion(INVALID_IDX), fmt.Errorf("Cannot compute QR version")
+}
+
+func GetModeIndicator(mode QrCodeMode) QrModeIndicator {
+	return modeIndicators[mode]
 }
 
 func main() {
--- qr_test.go
+++ qr_test.go
@@ -9,6 +9,7 @@ import (
 func TestNumericInputOnly(t *testing.T) {
 	assert := assert.New(t)
 	var input string
+	var err error
 
 	input = "1234"
 	actual, _ := GetMode(input)
@@ -28,12 +29,18 @@ func TestNumericInputOnly(t *testing.T) {
 
 	input = ""
 	actual, _ = GetMode(input)
-	assert.Equal(INVALID_MODE, actual, "Input pattern should be invalid")
+	assert.Equal(QrCodeMode(EMPTY_STRING), actual, "Input pattern should be invalid")
+
+	input = "こんにちは"
+	actual, err = GetMode(input)
+	assert.Equal(QrCodeMode(EMPTY_STRING), actual, "Input pattern should be invalid")
+	assert.Error(err)
 }
 
 func TestAlphaNumericInputOnly(t *testing.T) {
 	assert := assert.New(t)
 	var input string
+	var err error
 
 	input = "ABC"
 	actual, _ := GetMode(input)
@@ -52,13 +59,15 @@ func TestAlphaNumericInputOnly(t *testing.T) {
 	assert.Equal(ALPHA_NUMERIC, actual, "Input pattern should be alpha numeric")
 
 	input = ""
-	actual, _ = GetMode(input)
-	assert.Equal(INVALID_MODE, actual, "Input pattern should be invalid")
+	actual, err = GetMode(input)
+	assert.Equal(QrCodeMode(EMPTY_STRING), actual, "Input pattern should be invalid")
+	assert.Error(err)
 }
 
 func TestByteInputOnly(t *testing.T) {
 	assert := assert.New(t)
 	var input string
+	var err error
 
 	input = "ABCabc123"
 	actual, _ := GetMode(input)
@@ -69,27 +78,58 @@ func TestByteInputOnly(t *testing.T) {
 	assert.Equal(BYTE, actual, "Input pattern should be byte")
 
 	input = ""
-	actual, _ = GetMode(input)
-	assert.Equal(INVALID_MODE, actual, "Input pattern should be invalud")
+	actual, err = GetMode(input)
+	assert.Equal(QrCodeMode(EMPTY_STRING), actual, "Input pattern should be invalid")
+	assert.Error(err)
 }
 
 func TestComputeQrVersion(t *testing.T) {
 	assert := assert.New(t)
 	var input string
+	var err error
 
 	input = "HELLO WORLD"
-	actual, _ := GetSmallestVersion(input, QUARTILE)
+	actual, _ := GetSmallestVersion(input, ALPHA_NUMERIC, QUARTILE)
 	assert.Equal(VERSION_1, actual, "Input should be version 1")
 
 	input = "HELLO THERE WORLD"
-	actual, _ = GetSmallestVersion(input, QUARTILE)
+	actual, _ = GetSmallestVersion(input, ALPHA_NUMERIC, QUARTILE)
 	assert.Equal(VERSION_2, actual, "Input should be version 2")
 
 	input = "12345"
-	actual, _ = GetSmallestVersion(input, MEDIUM)
+	actual, _ = GetSmallestVersion(input, NUMERIC, MEDIUM)
 	assert.Equal(VERSION_1, actual, "Input should be version 1")
 
 	input = "https://www.google.com"
-	actual, _ = GetSmallestVersion(input, HIGH)
+	actual, _ = GetSmallestVersion(input, BYTE, HIGH)
 	assert.Equal(VERSION_3, actual, "Input should be version 3")
+
+	input = "this is a very long text fragment for which we cannot compute a compatible qr version" +
+		"this is a very long text fragment for which we cannot compute a compatible qr version" +
+		"this is a very long text fragment for which we cannot compute a compatible qr version"
+	actual, err = GetSmallestVersion(input, BYTE, LOW)
+	assert.Error(err)
+}
+
+func TestGetModeIndicator(t *testing.T) {
+	assert := assert.New(t)
+	var input string
+
+	input = "1233"
+	mode, _ := GetMode(input)
+	assert.Equal(NUMERIC, mode, "Input should be numeric")
+	actual := GetModeIndicator(mode)
+	assert.Equal(NUMERIC_INDICATOR, actual, "Input should have a numeric indicator")
+
+	input = "HELLO WORLD"
+	mode, _ = GetMode(input)
+	assert.Equal(ALPHA_NUMERIC, mode, "Input should be alpha numeric")
+	actual = GetModeIndicator(mode)
+	assert.Equal(ALPHA_NUMERIC_INDICATOR, actual, "Input should have a alpha numeric indicator")
+
+	input = "Hello, world!"
+	mode, _ = GetMode(input)
+	assert.Equal(BYTE, mode, "Input should be byte")
+	actual = GetModeIndicator(mode)
+	assert.Equal(BYTE_INDICATOR, actual, "Input should have a byte indicator")
 }
