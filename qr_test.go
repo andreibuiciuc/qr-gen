14b72b90bc641aa1e07f9994c98836d98fc8ec67
Andrei Buiciuc <abuiciuc33@gmail.com>
2023-12-30 17:23:45 +0200

    add one more test for alphanumeric encoding

--- qr_test.go
+++ qr_test.go
@@ -190,6 +190,10 @@ func TestAlphaNumericEncoding(t *testing.T) {
 	input = "HED"
 	actual = e.EncodeAlphanumericInput(input)
 	assert.Equal("01100001011001101", actual, "Input should match binary representation")
+
+	input = "HELLO WORLD"
+	actual = e.EncodeAlphanumericInput(input)
+	assert.Equal("0110000101101111000110100010111001011011100010011010100001101", actual, "Input should mathc binary representation")
 }
 
 func TestByteNumericInput(t *testing.T) {
